c5a4fdc894ed9b5cd526a45183a9f393a712075e
jeremie klinger <jeremie.klinger@gmail.com>
2022-09-09 18:48:16 +0200

    functionnal code.

--- arxiv_sorter.py
+++ arxiv_sorter.py
@@ -5,68 +5,19 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-import email
-from email.message import EmailMessage
-import subprocess
-# largest possible scope, necessary to delete emails.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
-
 #my own imports for string parsing
 import re
-import base64
 import toml
-
-
-def retrieve_unread_arxiv_email(service):
-        # Call the Gmail API
-    # this is the querying string, works like the gmail query box
-    myquery = 'in:inbox is:unread subject:physics daily'
-
-    messages=[]
-    results = service.users().messages().list(userId='me',q=myquery).execute()
-    if 'messages' in results:
-        messages.extend(results['messages'])
-    while 'nextPageToken' in results:
-        results = service.users().messages().list(userId='me',q=myquery, pageToken=results['nextPageToken']).execute()
-        if 'messages' in results:
-            messages.extend(results['messages'])
-    return messages
-
-def get_email_contents(service, message):
-    results=service.users().messages().get(userId='me',id=message['id'],format='full').execute()
-    message_text = results['payload']['body']['data']
-    message_text = base64.b64decode(message_text).decode("utf-8")
-    return message_text
-
-def extract_title(string):
-    match = re.search('Title: (.*)(Authors)',string,re.DOTALL) 
-    if match:
-        return match.group(1)
-    else:
-        print(string)
-        return None
-
-def extract_authors(string):
-    match = re.search('Authors: (.*)(Cat)',string,re.DOTALL) 
-    if match:
-        return match.group(1)
-    else:
-        return None
+import feedparser
+import requests
 
 class arxiv_article:
-    def __init__(self, id, t,a,c):
+    def __init__(self, id, t,a,c,d):
         self.id=id
         self.title = t
         self.authors=a
         self.contents=c
+        self.date=d
 
     # says if an email is relevant
     def is_relevant(self, dic):
@@ -75,16 +26,27 @@ class arxiv_article:
         return title_match or authors_match
 
     def format(self):
-        return f'{self.id}\n{self.contents}'
+        return f'{self.id}\n\nDate : {self.date}\n\nTitle : {self.title}\n\nSummary : {self.contents}\n\nAuthors : {self.authors}'
 
+def retrieve_latest_articles(latest_id):
+    cat="cond-mat.stat-mech"
+    
+    #by default we will query 50 articles 
+    api_url = f"http://export.arxiv.org/api/query?search_query=cat:{cat}&sortBy=submittedDate&sortOrder=descending&max_results=50"
+    response = requests.get(api_url)
+    parsed_response=feedparser.parse(response.text)
 
-def parse_email_content(contents):
-    arxiv_id = re.findall(r'^\\\\\r\narXiv:[0-9]{4}.*$',contents, flags=re.M)
-    contents = re.split(r'^\\\\\r\narXiv:[0-9]{4}.*$',contents, flags=re.M)[1:]
-    titles = [extract_title(c) for c in contents]
-    authors = [extract_authors(c) for c in contents]
-    arxiv_articles=[arxiv_article(id.split('\n')[1],t,a,c) for id,t,a,c in zip(arxiv_id,titles,authors,contents)]
-    return arxiv_articles
+    latest_articles = []
+    for entry in parsed_response.entries:
+        if entry.id==latest_id:
+            break
+        authors_name=[]
+        for author in entry.authors:
+            authors_name.append(author.name)
+        authors = ", ".join(authors_name)
+        latest_articles.append(arxiv_article(entry.id, entry.title,authors, entry.summary,' '.join(entry.updated.split('T'))))
+    return latest_articles
+
 
 #sending an email with sendgrid
 def send_email(contents,sender_adress,receive_adress):
@@ -100,98 +62,42 @@ def send_email(contents,sender_adress,receive_adress):
     #print(response.body)
     #print(response.headers)
 
-#sending email with himalaya
-def send_email_himalaya(contents, send_adress,receive_adress):
-    mail_template = f"""Content-Type: text/plain; charset=utf-8
-From: 'jeremie.klinger.pro' <{send_adress}>
-To: <{receive_adress}>
-Subject: Daily News
-
-Hey, here's your daily scientific news ! 
-
-{contents}
-"""
-    with open('template.mail','w') as f:
-        f.write(mail_template)
-
-    subprocess.run(["himalaya","send","<","template.mail"])
-
-   
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
 
-    service = build('gmail', 'v1', credentials=creds)
+    #retrieve useful email adresses
+    with open('email_adress.txt') as f:
+        lines=f.readlines()
+    send_adress = lines[0]
+    receive_adress = lines[1]
+    
+    #retrieve article selection
+    selection = toml.load('sorter.toml')
 
-    """
-    try:
-        # load toml setup file
-        parsed_toml = toml.load('sorter.toml')
+    # find old id
+    with open('old_id.txt') as f:
+        latest_id = f.readlines()[0]
 
-        # adress to send to, specified in 'email_adress.txt' file
-        # the sender adress is the one needed from sendgrid
-        with open('email_adress.txt') as f:
-            sender_adress=f.readline()[:-1]
-            receive_adress=f.readline()[:-1]
-        print(sender_adress)
+    # query until old id and parse into article format
+    latest_articles = retrieve_latest_articles(latest_id)
 
 
-        """
-        # treat emails
-        messages = retrieve_unread_arxiv_email(service)
+    # sort articles
+    relevant_articles = []
+    for article in latest_articles:
+        if article.is_relevant(selection):
+            relevant_articles.append(article)
+    if len(relevant_articles)==0:
+        contents="No news today !"
+    else:
+        # update old_id
+        with open('old_id.txt','w') as f:
+            f.write(relevant_articles[0].id)
 
-        #if no new emails, return
-        if len(messages)==0:
-            print('no new messages')
-            return
+        del_article = "\n\n----------------------------------------------------------------------------------\n\n"
+        contents = del_article.join([a.format() for a in relevant_articles])
 
-        #store relevant articles to send
-        relevant_articles=[]
-        email_contents="Salut Jerem, voici ton daily load of scientific news :)\n\n\n"
+    send_email(contents, send_adress, receive_adress)
 
-        for m in messages:
-            contents = get_email_contents(service, m)
-
-            arxiv_articles = parse_email_content(contents)
-            # sort the relevant ones
-            for article in arxiv_articles:
-                if article.is_relevant(parsed_toml):
-                    relevant_articles.append(article)
-
-        if len(relevant_articles)>0:
-            for article in relevant_articles:
-                email_contents+=article.format()
-
-            #send_email(email_contents,sender_adress,receive_adress)
-        """
-
-        email_contents="Salut Jerem, voici ton daily load of scientific news :)\n\n\n"
-        send_email_himalaya(email_contents, sender_adress, receive_adress)
-
-
-    except HttpError as error:
-        print('lala')
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
 
 
 if __name__ == '__main__':
